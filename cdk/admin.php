247e4465f1c424bf7f0b5dd796d37294b742b301
Mryam Jalali <jalali@sis-eg.com>
2015-03-16 17:46:10 +0330

    jalali bug fix2

--- cdk/admin.php
+++ cdk/admin.php
@@ -4,6 +4,10 @@
 // http://www.sis-eg.com/
 // ----------------------------------------------------------------------
 
+
+
+
+
 function cdk_admin_main() {	
 	pnRedirect('index.php?module=cdk&func=loadmodule&system=cdk&sismodule=configuration/content_types_list.php&_menu_=0');	
 	//pnRedirect(pnModURL('cdk', 'user', 'loadmodule', array('system'=>'cdk', 'sismodule'=>'configuration___content_types_list.php')));
